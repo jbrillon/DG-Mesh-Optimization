95ede8d380b1e19c9de77cd69285042eb653e338
jbrillon <julien.brillon@mail.mcgill.ca>
2021-12-18 14:18:42 -0500

    removed commented p4 lines

--- tests/h_convergence/run_h_convergence.sh
+++ tests/h_convergence/run_h_convergence.sh
@@ -15,22 +15,18 @@ do
     # inputs (ordered): ConvStudyFlag, P, Nel, ConvStudyType (0==p, 1==h)
     for i in $(seq 2 4 100)
     do
-        # echo -e "1\n4\n$i\n1\n" | ./1D_DG_solver_01.exe
         echo -e "1\n$P\n$i\n1\n$SetupFile\n$ParametersFile\n" | ./1D_DG_solver_01.exe
     done
     for i in $(seq 100 10 200)
     do
-        # echo -e "1\n4\n$i\n1\n" | ./1D_DG_solver_01.exe
         echo -e "1\n$P\n$i\n1\n$SetupFile\n$ParametersFile\n" | ./1D_DG_solver_01.exe
     done
     for i in $(seq 220 20 300)
     do
-        # echo -e "1\n4\n$i\n1\n" | ./1D_DG_solver_01.exe
         echo -e "1\n$P\n$i\n1\n$SetupFile\n$ParametersFile\n" | ./1D_DG_solver_01.exe
     done
     for i in $(seq 350 50 500)
     do
-        # echo -e "1\n4\n$i\n1\n" | ./1D_DG_solver_01.exe
         echo -e "1\n$P\n$i\n1\n$SetupFile\n$ParametersFile\n" | ./1D_DG_solver_01.exe
     done
     # copy results to data directory and rename appropriately
